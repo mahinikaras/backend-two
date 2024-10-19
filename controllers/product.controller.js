3b29524ccbb77d1f5624b07992f39488b7800e44
mahinikaras <mahin.ikaras123@gmail.com>
2024-10-19 16:50:11 +0600

    cache control with max age and etag

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,13 +1,51 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Product = require('../models/product.model.js');
+const crypto = require('crypto');
+const product = require('../models/product.model.js');
 const app = express();
 app.use(express.json());
 
 
-const getProducts = async (req, res) => {
+var products;
+function generateETag(data) {
+    const hash = crypto.createHash('md5').update(JSON.stringify(data)).digest('hex');
+    return `${hash}`; // Return the hash wrapped in quotes (standard for ETag)
+}
+
+
+
+
+async function cacheControlMiddleWare(req, res, next) {
+    console.log("cacheControlMiddleWare is called");
     try {
-        const products = await Product.find({});
+        products = await Product.find({});
+
+        const generatedETag = generateETag(products);
+        console.log(">>>> generate etag : ", generatedETag);
+        console.log(">>>> req.headers['if-none-match'] : ", req.headers['if-none-match']);
+
+        // Check if the client sent the If-None-Match header
+        if (req.headers['if-none-match'] === generatedETag) {
+            // If ETags match, send 304 Not Modified
+            return res.status(304).end();
+        }
+
+        // Otherwise, set the ETag header and continue to the next middleware
+        res.setHeader('ETag', generatedETag);
+
+        // Cache Control headers
+        res.setHeader('Cache-Control', 'public, max-age=30');
+        next();
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+const getProducts = async (req, res) => {
+    product
+    try {
+        //const products = await Product.find({});
         res.status(200).json(products);
     } catch (err) {
         res.status(500).json({ message: err.message });
@@ -32,4 +70,4 @@ const createProduct = async (req, res) => {
     }
 }
 
-module.exports = { getProducts, getproductbyId, createProduct }
+module.exports = { cacheControlMiddleWare, getProducts, getproductbyId, createProduct }
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const Product = require('../models/product.model');
-const { getProducts, getproductbyId, createProduct } = require('../controllers/product.controller');
+const crypto = require('crypto');
+const { cacheControlMiddleWare, getProducts, getproductbyId, createProduct } = require('../controllers/product.controller');
 
-router.get('/', getProducts)
 
-router.get('/:id', getproductbyId)
 
-router.post('/', createProduct)
+router.get('/', cacheControlMiddleWare, getProducts);
+
+router.get('/:id', getproductbyId);
+
+router.post('/', createProduct);
 
 module.exports = router
